61a0b547284a39565745489fb09a3a7365bd0f4d
billk <ikon321@yahoo.com>
2017-02-28 12:01:25 -0800

    removed domain from seeion.php

--- config/session.php
+++ config/session.php
@@ -148,7 +148,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'admin-test.com'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
